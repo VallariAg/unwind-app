3806d070bbdc7b2294985e173d1df51e5b53f712
VallariAg <val.agl002@gmail.com>
2021-05-30 03:10:47 +0530

    fix usePostNotes

--- hooks/usePostNotes.js
+++ hooks/usePostNotes.js
@@ -1,28 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 
 
 // Input format of "date" parameter is String. 
-export default function usePostNotes(data) {
-    const { loading, error, data } = useQuery(POST_NOTES,  {
-        variables: { 
-            data: data
-    }});
+export default function usePostNotes() {
+  const [ setNote ] = useMutation(POST_NOTES)
 
-    if (loading) return [];
-    if (error) return [];
-
-    return (data.notes)
+  return setNote;
 }
 
 const POST_NOTES = gql`
-mutation MyQuery($data: String) {
-  insert_notes(objects: {data: $data}) {
+mutation POST_NOTES($data: String, $noteType: String) {
+  insert_notes(objects: {data: $data, noteType: $noteType}) {
     returning {
       data
       time
-    }
-  
+      noteType
+    } 
 }
-
 `
