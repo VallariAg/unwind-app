8bf8fe589bbc8fec2086b4cc153fee7cdeee5432
Tanisha Jain <56258254+tanisha-uni@users.noreply.github.com>
2021-05-30 02:23:29 +0530

    update tabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { View, Text, Animated, StyleSheet, Image, TouchableOpacity} from 'react-native'
-// import {NavigationContainer, useNavigation, useRoute} from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-// import {focused} from '@react-navigation/native'
 import FeedNavigator from './FeedNavigator'
 import HomePageNavigator from './HomePageNavigator'
 import CalendarNavigator from './CalendarNavigator'
 import DiariesNavigator from './DiariesNavigator'
-// import Icon from 'react-native-vector-icons/FontAwesome';
-// import { TouchableOpacity } from 'react-native-gesture-handler'
-// import AddButton from '../components/AddButton'
-
-
 
 const Tab = createBottomTabNavigator()
  
@@ -22,38 +15,14 @@ export default function TabNavigator() {
       tabBarOptions={{
         showLabel: false,
         style: {
-          position: 'absolute',
-          bottom: 25,
-          left: 20,
-          right: 20,
+          position: 'relative',
           elevation: 0,
           borderRadius: 15,
           backgroundColor: 'purple',
           height: 70,
-          ...styles.shadow
         },
       }}
     >
-
-    <Tab.Screen
-        name = "Calendar"
-        component={CalendarNavigator}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <View>
-              <Image
-                source={require('../assets/Icons/calendar.svg')}
-                resizeMode = 'contain'
-                style={{                
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? 'black' : 'white'
-                }}
-              />
-            </View>
-          )
-        }}
-      />
     <Tab.Screen
         name = "HomePage"
         component={HomePageNavigator}
@@ -116,15 +85,3 @@ export default function TabNavigator() {
   )
 }
 
-const styles = StyleSheet.create({
-  shadow: {
-    shadowColor: 'gray',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: .25,
-    shadowRadius: 3.5,
-    elevation: 5,
-  },
-})
